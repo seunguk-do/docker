e79ce34a94f3669673f6c4c6fa06a2e74e3e09c6
seunguk-do <seunguk.d@gmail.com>
2023-05-25 23:17:13 +0900

    Edit run_container.sh

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -2,14 +2,14 @@
 container_name=$1
 image=$2
 version=$3
-project_dir=$4
+code_dir=$4
 data_dir=$5
 user=$USER
 cpus=$6
 echo "containe_name: $container_name"
 echo "image: $image"
 echo "version: $version"
-echo "project_dir: $project_dir"
+echo "code_dir: $code_dir"
 echo "data_dir: $data_dir"
 echo "user: $user"
 docker run \
@@ -20,7 +20,7 @@ docker run \
 	--net=host \
 	--shm-size=512gb \
 	--cpuset-cpus=$cpus \
-	-v $project_dir:/home/$user/Projects \
+	-v $code_dir:/home/$user/Codes \
 	-v $data_dir:/home/$user/Data \
 	-v $HOME/.ssh:/home/$user/.ssh \
 	--workdir=/home/$user \
