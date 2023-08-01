e256492d2df2f4e3821409d6427e9b316ecf163a
seunguk-do <seunguk.d@gmail.com>
2023-08-01 18:05:46 +0900

    Edit nvim

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -2,14 +2,14 @@
 container_name=$1
 image=$2
 version=$3
-code_dir=$4
+proj_dir=$4
 data_dir=$5
 user=$USER
 cpus=$6
 echo "containe_name: $container_name"
 echo "image: $image"
 echo "version: $version"
-echo "code_dir: $code_dir"
+echo "proj_dir: $proj_dir"
 echo "data_dir: $data_dir"
 echo "user: $user"
 docker run \
@@ -20,8 +20,8 @@ docker run \
 	--net=host \
 	--shm-size=512gb \
 	--cpuset-cpus=$cpus \
-	-v $code_dir:/home/$user/Codes \
-	-v $data_dir:/home/$user/Data \
+	-v $proj_dir:/home/$user/projects \
+	-v $data_dir:/home/$user/data \
 	-v $HOME/.ssh:/home/$user/.ssh \
 	--workdir=/home/$user \
 	--privileged \
